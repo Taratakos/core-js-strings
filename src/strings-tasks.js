fe98691ac34a151d0f0758431541f695fe7014d2
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-11-15 09:32:48 +0200

    feat: add solution for first 24 tasks (endsWith), Wed Nov 15 2023 09:32:03 GMT+0200 (Eastern European Standard Time)

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -175,8 +175,13 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  // визначаємо індекс останнього входження підстроки в рядку str
+  const index = str.lastIndexOf(value);
+  //  slice повертає частину рядка від початку до індексу перд останнім входжженням (не вкл його) і частину після jcnfyymjuj входження починаючи з індексу, який вказує на кінець входження і додаючи довжину підстроки, яку ми хочемо вилучити
+  return index !== -1
+    ? str.slice(0, index) + str.slice(index + value.length)
+    : str;
 }
 
 /**
@@ -191,8 +196,17 @@ function removeLastOccurrences(/* str, value */) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (typeof str !== 'string') {
+    return 0; // Повертаємо 0, якщо введений параметр не є рядком.
+  }
+
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i); // додаємо код кожного символу до суми
+  }
+
+  return sum;
 }
 
 /**
@@ -206,8 +220,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -221,8 +235,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
